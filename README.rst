9e654f2b6e3d628cc79fa2be6025e2d2b72f04c3
Lockefox <locke.renard@gmail.com>
2018-11-21 16:00:21 -0800

    adding prosper setup.py standards

---
+++ README.rst
@@ -0,0 +1,40 @@
+|Show Logo|
+
+ProsperForecast 
+===============
+
+|Build Status| |Coverage Status| |Docs|
+
+Flask App for generating experimental stock forecasts
+
+Getting Started
+---------------
+
+.. code-block::
+
+    git clone git@github.com:EVEprosper/ProsperForecast.git
+    pip3 install -e .
+    # TODO: docker launcher
+
+
+Testing
+-------
+
+.. code-block::
+
+    python setup.py test
+
+Deployment
+----------
+
+TODO
+
+.. |Show Logo| image:: http://dl.eveprosper.com/podcast/logo-colour-17_sm2.png
+   :target: http://eveprosper.com
+.. |Build Status| image:: https://travis-ci.org/EVEprosper/ProsperForecast.svg?branch=master
+    :target: https://travis-ci.org/EVEprosper/ProsperForecast
+.. |Coverage Status| image:: https://coveralls.io/repos/github/EVEprosper/ProsperForecast/badge.svg?branch=master
+    :target: https://coveralls.io/github/EVEprosper/ProsperForecast?branch=master
+.. |Docs| image:: https://readthedocs.org/projects/prosperforecast/badge/?version=latest
+   :target: http://prosperforecast.readthedocs.io/en/latest/?badge=latest
+   :alt: Documentation Status
--- setup.py
+++ setup.py
@@ -1,13 +1,89 @@
+"""wheel setup for ProsperForecast"""
+import codecs
+import importlib
+
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
-__version__ = '0.1'
+__package_name__ = 'ProsperForecast'
+__library_name__ = 'forecast'
 
+def get_version(*args):
+    """find __version__ for making package
+
+    Args:
+        package_name (str): path to _version.py folder (abspath > relpath)
+
+    Returns:
+        str: __version__ value
+
+    """
+    module = f'{".".join(args)}._version'
+    package = importlib.import_module(module)
+
+    version = package.__version__
+
+    return version
+
+class PyTest(TestCommand):
+    """PyTest cmdclass hook for test-at-buildtime functionality
+
+    http://doc.pytest.org/en/latest/goodpractices.html#manual-integration
+
+    """
+    user_options = [('pytest-args=', 'a', 'Arguments to pass to pytest')]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = [
+            'tests',
+            '-rx',
+            f'--cov={__library_name__}',
+            '--cov-report=term-missing',
+            '--cov-config=.coveragerc',
+        ]
+
+    def run_tests(self):
+        import shlex
+        import pytest
+        pytest_commands = []
+        try:
+            pytest_commands = shlex.split(self.pytest_args)
+        except AttributeError:
+            pytest_commands = self.pytest_args
+        errno = pytest.main(pytest_commands)
+        exit(errno)
+
+class QuietTest(PyTest):
+    """overrides to prevent webhook spam while developing"""
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = [
+            'tests',
+            '-rx',
+            f'--cov={__library_name__}',
+            '--cov-report=term-missing',
+            '--cov-config=.coveragerc',
+        ]
+
+with codecs.open('README.rst', 'r', 'utf-8') as readme_fh:
+    README = readme_fh.read()
 
 setup(
-    name='forecast',
-    version=__version__,
+    name=__package_name__,
+    author='John Purcell',
+    author_email='prospermarketshow@gmail.com',
+    version=get_version(__library_name__),
     packages=find_packages(exclude=['tests']),
+    description='Flask App for generating experimental stock forecasts',
+    long_description=README,
+    package=find_packages(),
+    package_data={
+        '': ['LICENSE', 'README.rst'],
+        'forecast': ['version.txt'],
+    },
     install_requires=[
+    ## cookiecutter requirements ##
         'flask',
         'flask-sqlalchemy',
         'flask-restful',
@@ -16,11 +92,37 @@ setup(
         'flask-marshmallow',
         'marshmallow-sqlalchemy',
         'python-dotenv',
-        'passlib'
+        'passlib',
+    ## Custom requirements ##
+        'ProsperCommon',
+        'ProsperDatareader',
+        'fbprophet',
+        'gunicorn',
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-prosper',
+        'docker',
+        'coverage',
+        'pytest_cov',
+        'factory_boy',
+        'pytest-factoryboy',
+        'pytest-flask',
+        'pytest-runner',
     ],
     entry_points={
         'console_scripts': [
             'forecast = forecast.manage:cli'
         ]
-    }
+    },
+    extras_require={
+        'dev': [
+            'sphinx',
+            'sphinxcontrib-napoleon',
+            'rstcheck',
+        ],
+    },
+    cmdclass={
+        'test': PyTest,
+    },
 )
